c0f8f1946b4f2e7d4fb02c49cfa0e396c5bfa096
nakyung2 <skruddl50@gmail.com>
2023-01-31 14:44:24 +0900

    Storybook 테마 설정

---
+++ .storybook/theme.js
@@ -0,0 +1,12 @@
+import logo from "../public/img/logo.png";
+import { create } from "@storybook/theming";
+
+export default create({
+  // theme dark or light
+//   base: "dark",
+
+  // brand logo
+  brandImage: logo,
+  brandName: "logo name",
+  brandUrl: "url"
+});
