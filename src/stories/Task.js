377e77240534fe1062d977bdd58ad35f9317a625
nakyung2 <skruddl50@gmail.com>
2023-02-09 17:38:06 +0900

    modify Task

--- src/stories/Task.js
+++ src/stories/Task.js
@@ -3,67 +3,160 @@ import PropTypes from "prop-types";
 // import './Task.css'
 
 export default function Task({
-  task: { id, title, state },
-  onArchiveTask,
-  onPinTask,
+  task:{id, title,archived,pinned},
+  // onArchiveTask,
+  // onPinTask
 }) {
-  return (
-    <div className={`list-item ${state}`}>
-      <label className="checkbox">
-        <input
-          type="checkbox"
-          defaultChecked={state === "TASK_ARCHIVED"}
-          disabled={true}
-          name="checked"
-        />
-        <span
-          className="checkbox-custom"
-          onClick={() => onArchiveTask(id)}
-          id={`archiveTask-${id}`}
-          aria-label={`archiveTask-${id}`}
-        />
-      </label>
-      <div className="title">
-        <input
+  return(
+    <div>
+      <label>
+        {
+          archived ? (
+       <div style={{display:"inline"}}>
+            <span>
+            <img src="img/check.png" width={15}/>
+           </span>
+           <input
           type="text"
           value={title}
           readOnly={true}
           placeholder="Input title"
+          style={{border:0, flex: 1, color: '#a0aec0', 
+            textDecoration: 'line-through'}}
         />
+        </div>
+          ) : (
+       <div style={{display:"inline"}}>
+            <input type="checkbox"
+            id={id}
+          /><input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+          style={{border:0, flex: 1}}
+        />
+          </div>
+          )
+        }
+      </label>
+
+       {/* <div style={{display:"inline"}}>
+         <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+          style={{border:0, flex: 1}}
+        />
+      </div> */}
+
+
+      <div style={{display:"inline"}}>
+           {/* <a onClick={() => onPinTask(id)}> */}
+        { pinned ? (
+            <img src="img/heart.png" width={15}/>
+        ) : (
+          <img src="img/heart_empty.png" width={15}/>
+        )}
+          {/* </a> */}
       </div>
 
-      {/* e.stopPropagation 이벤트의 상위 전달 막기 => e.preventDefault 이벤트 동작 막기  */}
-      <div className="actions" onClick={(event) => event.stopPropagation()}>
-        {state !== "TASK_ARCHIVED" && (
-          // eslint-disable-next-line jsx-a11y/anchor-is-valid
-          <a onClick={() => onPinTask(id)}>
-            <span
-              className={`icon-star`}
-              id={`pinTask-${id}`}
-              aria-label={`pinTask-${id}`}
-            />
-          </a>
-        )}
-      </div>
+
     </div>
-  );
+  )
 }
 
-// 데이터 요구 사항 명시하기
-// 컴포넌트에 필요한 데이터 형태 명시
-// 문서화, 문제 조기 발견에 용이
-Task.propTypes = {
-  /** task */
+// prop 타입 정의
+Task.propTypes={
   task: PropTypes.shape({
-    /** 아이디 */
-    id: PropTypes.string.isRequired,
-    /** 제목 */
-    title: PropTypes.string.isRequired,
-    /** 상태 */
-    state: PropTypes.string.isRequired,
-  }),
-  /** 완료 여부 */
-  onArchiveTask: PropTypes.func,
-  /** 중요 여부 */
-  onPinTask: PropTypes.func,
-};
+        /** 아이디 */
+        id: PropTypes.string.isRequired,
+        /** 제목 */
+        title: PropTypes.string.isRequired,
+        // /** 상태 */
+        // state: PropTypes.oneOf(['do','done']).isRequired,
+        /** 완료여부 */
+        archived: PropTypes.bool,
+        /** 중요여부 */
+        pinned:PropTypes.bool
+
+      }),
+        // /** 완료 여부 함수 */
+        // onArchiveTask: PropTypes.func,
+        // /** 중요 여부 함수 */
+        // onPinTask: PropTypes.func,
+}
+
+Task.defaultProps = {
+  id:'1',
+  title:'task',
+  archived: false,
+  pinned: false
+}
+
+// export default function Task({
+//   task: { id, title, state },
+//   onArchiveTask,
+//   onPinTask,
+// }) {
+//   return (
+//     <div className={`list-item ${state}`}>
+//       <label className="checkbox">
+//         <input
+//           type="checkbox"
+//           defaultChecked={state === "TASK_ARCHIVED"}
+//           disabled={true}
+//           name="checked"
+//         />
+//         <span
+//           className="checkbox-custom"
+//           onClick={() => onArchiveTask(id)}
+//           id={`archiveTask-${id}`}
+//           aria-label={`archiveTask-${id}`}
+//         />
+//       </label>
+//       <div className="title">
+//         <input
+//           type="text"
+//           value={title}
+//           readOnly={true}
+//           placeholder="Input title"
+//         />
+//       </div>
+
+//       {/* e.stopPropagation 이벤트의 상위 전달 막기 => e.preventDefault 이벤트 동작 막기  */}
+//       <div className="actions" onClick={(event) => event.stopPropagation()}>
+//         {state !== "TASK_ARCHIVED" && (
+//           // eslint-disable-next-line jsx-a11y/anchor-is-valid
+//           <a onClick={() => onPinTask(id)}>
+//             <span
+//               className={`icon-star`}
+//               id={`pinTask-${id}`}
+//               aria-label={`pinTask-${id}`}
+//             />
+//           </a>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// // 데이터 요구 사항 명시하기
+// // 컴포넌트에 필요한 데이터 형태 명시
+// // 문서화, 문제 조기 발견에 용이
+// Task.propTypes = {
+//   /** task */
+//   task: PropTypes.shape({
+//     /** 아이디 */
+//     id: PropTypes.string.isRequired,
+//     /** 제목 */
+//     title: PropTypes.string.isRequired,
+//     /** 상태 */
+//     state: PropTypes.string.isRequired,
+//   }),
+//   /** 완료 여부 */
+//   onArchiveTask: PropTypes.func,
+//   /** 중요 여부 */
+//   onPinTask: PropTypes.func,
+// };
--- src/stories/Task.stories.js
+++ src/stories/Task.stories.js
@@ -3,35 +3,71 @@ import React from 'react';
 import Task from './Task';
 
 export default {
-    component: Task,
-    title: 'components/Task',
-};
+     // 해당 컴포넌트
+     component: Task,
+    // 스토리북 앱의 사이드바에서 컴포넌트를 참조하는 방법
+     title: 'components/Task',
+}
 
-const Template = (args) => <Task {...args} /> ;
+// 순열(permutations)이 여러 개이기 때문에 Template 변수에 할당하는 것이 편리
+// args의 값이 변하면 컴포넌트도 함께 변함
+const Template = (args) => <Task {...args} />;
 
+// 함수의 복사본을 만드는 표준 JavaScript의 한 기법
+// 각각의 스토리가 고유한 속성(properties)을 갖지만 동일한 결과물을 사용하도록
 export const Default = Template.bind({});
-
-Default.args = {
-    task: {
-        id: '1',
-        title: 'Test task',
-        state: 'TASK_INBOX',
-        updateAt: new Date(2023, 0, 1, 9, 0)
-    },
+Default.args={
+    task:{
+        id:'1',
+        title: "default"
+        // state:'default'
+    }
 };
 
 export const Pinned = Template.bind({});
-Pinned.args = {
-    task : {
-        ...Default.args.task,
-        state: 'TASK_PINNED',
-    },
+Pinned.args={
+    task:{
+        id:'2',
+        title: "pinned",
+        pinned: true,
+                ...Default.args.task,
+                // state:'pinned'
+    }
 };
 
 export const Archived = Template.bind({});
-Archived.args = {
+Archived.args={
     task:{
-        ...Default.args.task,
-        state: 'TASK_ARCHIVED',
-    },
-};
+        id:'3',
+        title: "archived",
+        archived: true,
+                ...Default.args.task,
+        // state:'archived'
+    }
+}
+
+// export const Default = Template.bind({});
+// Default.args = {
+//     task: {
+//         id: '1',
+//         title: 'Test task',
+//         state: 'TASK_INBOX',
+//         updateAt: new Date(2023, 0, 1, 9, 0)
+//     },
+// };
+
+// export const Pinned = Template.bind({});
+// Pinned.args = {
+//     task : {
+//         ...Default.args.task,
+//         state: 'TASK_PINNED',
+//     },
+// };
+
+// export const Archived = Template.bind({});
+// Archived.args = {
+//     task:{
+//         ...Default.args.task,
+//         state: 'TASK_ARCHIVED',
+//     },
+// };
